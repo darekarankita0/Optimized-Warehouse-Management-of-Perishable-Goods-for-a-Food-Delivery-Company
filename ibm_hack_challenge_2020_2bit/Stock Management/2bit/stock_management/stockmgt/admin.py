a288cda011d456b3591e1c8a6806b7047ed3441d
darekarankita0 <55353280+darekarankita0@users.noreply.github.com>
2020-07-15 05:06:12 +0530

    Add files via upload

---
+++ ibm_hack_challenge_2020_2bit/Stock Management/2bit/stock_management/stockmgt/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .forms import StockCreateForm
+from .models import *
+
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity', 'issue_by']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+admin.site.register(Stock, StockCreateAdmin)
+admin.site.register(Category)
